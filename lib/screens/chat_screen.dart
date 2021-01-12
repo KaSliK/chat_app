f002ed7f47143a1eb1b27090a6ae6483a01b1588
KaSliK <kamil.kaslikowski@gmail.com>
2021-01-12 22:33:19 +0100

    Making progress with displaying data from firestore and adding new messages

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -6,26 +6,32 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text('This works!'),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          Firestore.instance
-              .collection('chats/FTUBHIbIoivpfsprYcXy/messages')
-              .snapshots()
-              .listen((data) {
-            data.documents.forEach((document) {
-              print(document['text']);
-            });
-          });
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection('chats/FTUBHIbIoivpfsprYcXy/messages')
+            .snapshots(),
+        builder: (ctx, streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final documents = streamSnapshot.data.documents;
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (ctx, index) => Container(
+              padding: EdgeInsets.all(8),
+              child: Text(documents[index]['text']),
+            ),
+          );
         },
       ),
+      floatingActionButton:
+          FloatingActionButton(child: Icon(Icons.add), onPressed: () {
+            Firestore.instance.collection('chats/FTUBHIbIoivpfsprYcXy/messages').add({
+              'text': 'This was added by clicking button'
+            });
+          }),
     );
   }
 }

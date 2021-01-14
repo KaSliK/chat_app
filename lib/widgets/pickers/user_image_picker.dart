f53803088749b739fc0fd64aaec8225cc328fb8d
KaSliK <kamil.kaslikowski@gmail.com>
2021-01-14 22:09:08 +0100

    Registering requires image to sing up, app know that user upload photo or not

--- lib/widgets/pickers/user_image_picker.dart
+++ lib/widgets/pickers/user_image_picker.dart
@@ -4,19 +4,23 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class UserImagePicker extends StatefulWidget {
+  UserImagePicker(this.imagePickFn);
+  final void Function(File pickedImage) imagePickFn;
+
   @override
   _UserImagePickerState createState() => _UserImagePickerState();
 }
 
 class _UserImagePickerState extends State<UserImagePicker> {
   File _pickedImage;
+
   void _pickImage() async {
     final pickedImageFile =
         await ImagePicker.pickImage(source: ImageSource.camera);
-        setState(() {
-           _pickedImage = pickedImageFile;
-        });
-
+    setState(() {
+      _pickedImage = pickedImageFile;
+    });
+    widget.imagePickFn(pickedImageFile);
   }
 
   @override
@@ -26,7 +30,8 @@ class _UserImagePickerState extends State<UserImagePicker> {
         CircleAvatar(
           radius: 40,
           backgroundColor: Colors.grey,
-          backgroundImage: _pickedImage != null ? FileImage(_pickedImage) : null,
+          backgroundImage:
+              _pickedImage != null ? FileImage(_pickedImage) : null,
         ),
         FlatButton.icon(
           textColor: Theme.of(context).primaryColor,

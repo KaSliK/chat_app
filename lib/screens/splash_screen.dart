bcdc626dec6e9ed877b2bdcbac8fc3861f919d93
KaSliK <kamil.kaslikowski@gmail.com>
2021-01-16 11:31:03 +0100

    Changed background color of splashScreen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Loading...'),
-    ),
-
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Center(
+        child: Text('Loading...'),
+      ),
     );
   }
 }
